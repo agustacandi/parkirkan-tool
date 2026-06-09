da66bc28cec1e72170d1ef130bca90adf8dc345d
Candi Agusta <mugiwaraagusta@gmail.com>
2026-06-09 14:54:33 +0700

    test

--- servo_control.py
+++ servo_control.py
@@ -1,19 +1,36 @@
-from gpiozero import Servo
+# from gpiozero import Servo
+# from time import sleep
+#
+# from config import SERVO_GPIO
+#
+# servo = Servo(SERVO_GPIO)
+#
+#
+# def open_gate():
+#
+#     print("OPENING GATE...")
+#
+#     servo.max()
+#
+#     sleep(2)
+#
+#     servo.min()
+#
+#     print("GATE CLOSED")
+
+from gpiozero import AngularServo
 from time import sleep
 
-from config import SERVO_GPIO
-
-servo = Servo(SERVO_GPIO)
+servo = AngularServo(
+    18,
+    min_angle=0,
+    max_angle=90
+)
 
 
 def open_gate():
+    servo.angle = 90
+    sleep(3)
 
-    print("OPENING GATE...")
-
-    servo.max()
-
-    sleep(2)
-
-    servo.min()
-
-    print("GATE CLOSED")
+    servo.angle = 0
+    sleep(1)
